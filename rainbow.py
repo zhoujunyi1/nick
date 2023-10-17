f6dd3831efe374738408b2a6537868f27c2b5ca5
zhoujunyi1 <74624844+zhoujunyi1@users.noreply.github.com>
2023-10-18 07:39:33 +0800

    Create rainbow.py

---
+++ rainbow.py
@@ -0,0 +1,51 @@
+import turtle
+turtle.pensize(10)
+# 绘制第一道彩虹
+turtle.pencolor("red")
+turtle.left(90)
+turtle.circle(10,180)
+# 绘制第二道彩虹
+turtle.pencolor("orange")
+turtle.left(180)
+turtle.penup()
+turtle.goto(10,0)
+turtle.pendown()
+turtle.circle(20,180)
+# 绘制第三道彩虹
+turtle.pencolor("yellow")
+turtle.left(180)
+turtle.penup()
+turtle.goto(20,0)
+turtle.pendown()
+turtle.circle(30,180)
+# 绘制第四道彩虹
+turtle.pencolor("green")
+turtle.left(180)
+turtle.penup()
+turtle.goto(30,0)
+turtle.pendown()
+turtle.circle(40,180)
+# 绘制第五道彩虹
+turtle.pencolor("blue")
+turtle.left(180)
+turtle.penup()
+turtle.goto(40,0)
+turtle.pendown()
+turtle.circle(50,180)
+# 绘制第六道彩虹
+turtle.pencolor("pink")
+turtle.left(180)
+turtle.penup()
+turtle.goto(50,0)
+turtle.pendown()
+turtle.circle(60,180)
+# 绘制第七道彩虹
+turtle.pencolor("purple")
+turtle.left(180)
+turtle.penup()
+turtle.goto(60,0)
+turtle.pendown()
+turtle.circle(70,180)
+
+
+turtle.done()
